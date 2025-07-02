e5f68094e9bc68eaddcb93ee7b65a1b81c407e84
devjeiku <jakebasa0428@gmail.com>
2025-07-02 20:40:50 +0800

    travel-list app starter

---
+++ 03-travel-list/src/App.js
@@ -0,0 +1,36 @@
+export default function App() {
+    return (
+        <div>
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
+}
+
+function Logo() {
+    return <h1>🌴Far Away💼</h1>;
+}
+
+function Form() {
+    return (
+        <div className='add-form'>
+            <h3>What do you need for your 😍 trip?</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+    return (
+        <footer>
+            <em>
+                💼 You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
