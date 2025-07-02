90c3aef926b6ef7d184e34231564c7485e4f9baa
devjeiku <jakebasa0428@gmail.com>
2025-07-02 21:28:33 +0800

    added logo and form ui

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,9 +1,15 @@
+const initialItems = [
+    { id: 1, description: 'Passports', quantity: 2, packed: false },
+    { id: 2, description: 'Socks', quantity: 12, packed: true },
+    { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
     return (
-        <div>
+        <div className='app'>
             <Logo />
             <Form />
-            <PackingList />
+            <PackingList items={initialItems} />
             <Stats />
         </div>
     );
@@ -15,15 +21,69 @@ function Logo() {
 
 function Form() {
     return (
-        <div className='add-form'>
+        <form className='add-form'>
             <h3>What do you need for your 😍 trip?</h3>
+            <select>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+            </select>
+            <input type='text' placeholder='Item...' />
+            <button>Add</button>
+        </form>
+    );
+}
+
+function PackingList({ items }) {
+    return (
+        <div className='list'>
+            <ul>
+                {items.map((item) => (
+                    <Item
+                        key={item.id}
+                        description={item.description}
+                        quantity={item.quantity}
+                        packed={item.packed}
+                    />
+                ))}
+            </ul>
         </div>
     );
 }
 
-function PackingList() {
-    return <div className='list'>LIST</div>;
+// function PackingList() {
+//     return (
+//         <div className='list'>
+//             <ul>
+//                 {initialItems.map((item) => (
+//                     <Item item={item} />
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+function Item({ key, description, quantity, packed }) {
+    return (
+        <li>
+            <span style={packed ? { textDecoration: 'line-through' } : {}}>
+                {quantity} {description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
 }
+
+// function Item({ item }) {
+//     return (
+//         <li>
+//             <span>
+//                 {' '}
+//                 {item.quantity} {item.description}
+//             </span>
+//         </li>
+//     );
+// }
 
 function Stats() {
     return (
