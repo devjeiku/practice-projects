ff5f95ae6a387e90e200ea8671e404c694aef59b
devjeiku <jakebasa0428@gmail.com>
2025-07-05 22:41:46 +0800

    add statistics on stats component

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -34,7 +34,7 @@ export default function App() {
                 onDeleteItem={handleDeleteItem}
                 onToggleItem={handleToggleItem}
             />
-            <Stats />
+            <Stats items={items} />
         </div>
     );
 }
@@ -158,11 +158,23 @@ function Item({
 //     );
 // }
 
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className='stats'>
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
     return (
         <footer className='stats'>
             <em>
-                💼 You have X items on your list, and you already packed X (X%)
+                {percentage === 100
+                    ? 'You got everything! Ready to go✈️'
+                    : `💼 You have ${numItems} on your list, and you already packed ${numPacked} (${percentage}%)`}
             </em>
         </footer>
     );
