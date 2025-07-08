2c9783c52b2dd76ae068095884a0c4b6908042cf
devjeiku <jakebasa0428@gmail.com>
2025-07-08 11:34:07 +0800

    used svg icons for the show/hide buttons

--- 04-accordion/src/components/Item.jsx
+++ 04-accordion/src/components/Item.jsx
@@ -13,7 +13,38 @@ export default function Item({ num, title, text }) {
                 <p className='number'>{num < 9 ? `0${num + 1}` : num + 1}</p>
                 <p className='title'>{title}</p>
                 <p className='icon' onClick={handleClick}>
-                    {isActive ? '+' : '-'}
+                    {isActive ? (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='24'
+                            height='24'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            stroke='currentColor'
+                            stroke-width='2'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            class='lucide lucide-plus-icon lucide-plus'
+                        >
+                            <path d='M5 12h14' />
+                            <path d='M12 5v14' />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            width='24'
+                            height='24'
+                            viewBox='0 0 24 24'
+                            fill='none'
+                            stroke='currentColor'
+                            stroke-width='2'
+                            stroke-linecap='round'
+                            stroke-linejoin='round'
+                            class='lucide lucide-minus-icon lucide-minus'
+                        >
+                            <path d='M5 12h14' />
+                        </svg>
+                    )}
                 </p>
                 <div className={`content-box ${isActive ? 'show' : ''}`}>
                     {text}
